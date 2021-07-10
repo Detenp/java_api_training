b87def76724fcffd64ecedd8a0ee78b37b24eae1
Lounes Bounouar <lounes.bounouar@epita.fr>
2021-07-10 15:13:38 +0200

    rollback pour correction dd'erreurs

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -17,6 +17,7 @@ import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 
 public class Launcher {
+    /*
     public static void main(String[] args) throws Exception {
         if (args.length < 1) throw new IllegalArgumentException("Expected at least port.");
         int port = Integer.parseInt(args[0]);
@@ -34,6 +35,62 @@ public class Launcher {
             } while(!success);
             players[1] = new Utils().playerFromJSonResponse(response);
             server.getIa().shoot();
+        }
+    }*/
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            throw new IllegalArgumentException("Expected at least port.");
+        }
+        String portStr = args[0];
+        int port = Integer.parseInt(portStr);
+
+        if (args.length > 1) {
+            String url = args[1];
+            Player[] players = new Player[2];
+            players[0] = new Player(port + "", "http://localhost:" + port);
+            IAForTheWin ia = new IAForTheWin(players);
+            Server server = new Server(port,port + "", players, ia);
+            server.start();
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url + "/api/game/start"))
+                .setHeader("Accept","application/json")
+                .POST(HttpRequest.BodyPublishers.ofString("{\"id\":\"" + port + "\", \"url\":\"http://localhost:"
+                    + port + "\", \"message\":\"hello\"}"))
+                .build();
+
+            HttpResponse<String> response = null;
+            boolean success = false;
+            do {
+                try {
+                    response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                    success = true;
+                } catch(ConnectException ignored){}
+            } while(!success);
+
+            JSONObject jo;
+            jo = (JSONObject) (new JSONParser()).parse(response.body());
+
+            String reqId = (String) jo.get("id");
+            String enemyUrl = (String) jo.get("url");
+            String message = (String) jo.get("message");
+
+            if (reqId == null || url == null || message == null) {
+                return;
+            }
+
+            players[1] = new Player(reqId, enemyUrl);
+
+            System.out.println("Battle is about to start between " + players[0].getUrl() + " and " + players[1].getUrl());
+            ia.shoot();
+        }
+        else {
+            Player[] players = new Player[2];
+            players[0] = new Player(port + "", "http://localhost:" + port);
+            Server server = new Server(port,port + "", players, new IAForTheWin(players));
+            server.start();
         }
     }
 }
